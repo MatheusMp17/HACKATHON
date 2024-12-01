90f8685e0dae6fb69919fc9ff7b192f5de4fdfbc
Matheus M Pedroso <matheusmorschbacher.mmp@gmail.com>
2024-12-01 02:26:11 -0300

    commit pessoadoadora

--- PessoaDoador.java
+++ PessoaDoador.java
@@ -1,5 +1,14 @@
 public class PessoaDoador extends Pessoa{
+    private Item doacao;
     private PessoaDoador(int id, Endereco enderecoPessoa, String email, Item doacao) {
-        super(id, enderecoPessoa, email, doacao);
+        super(id, enderecoPessoa, email);
+        setDoacao(doacao);
+    }
+    public void setDoacao(Item doacao){
+        this.doacao = doacao;
+    }
+    public Item getDoacao() {
+        return doacao;
     }
 }
+
