580f2f8c4204581e8af7782fe07514a8d4dbaed8
mizuki <130196354+PogMizuki@users.noreply.github.com>
2024-12-01 02:13:42 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int id, usuario;
+        System.out.print("Gostaria de entrar como instituição ou pessoa?\n1- pessoa\n2- instituição");
+        usuario = teclado.nextInt();
+
+        if(usuario == 1) {
+            System.out.print("Informe o seu CPF: ");
+            id = teclado.nextInt();
+            System.out.print("");
+        }
+    }
+}
+
+     
+   // public static ArrayList<String> ItemDoacao = new ArrayList<>();
+           //       ArrayList<String> ItemNecessario = new ArrayList<>();
+
     
 }
+
