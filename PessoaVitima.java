74276f095d0bd671ae69a20bc66ad63517546a8a
Henriquerike14_ <nylandrike@gmail.com>
2024-12-01 00:55:27 -0300

    PessoaVitima

---
+++ PessoaVitima.java
@@ -0,0 +1,5 @@
+public class PessoaVitima extends Pessoa{
+    private PessoaVitima(int id, Endereco enderecoPessoa, String email, Doacao doacao) {
+        super(id, enderecoPessoa, email, doacao);
+    }
+}
