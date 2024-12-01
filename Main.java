70a670f61ed185dea6eedcbd2c8e17fd00c5b665
BrunoJanesIV <brunojanes134191@gmail.com>
2024-12-01 01:21:32 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,9 @@
 public class Main {
+
+}
+
+
+
 /*    A ferramenta tem o objetivo de facilitar a prefeitura no processo de doações e pedidos de ajuda entre doadores, vítimas e instituições. 
 O processo começa com o cadastro da pessoa ou instituições, usando números de identidade (será explicado depois), endereço e doação. A 
 partir daí, a pessoa ou instituição seleciona se é uma vítima ou doador, depois determinar se o número de identidade é para um CPF ou
@@ -8,4 +13,3 @@ doadora diz os materiais que pode entregar às vítimas, enquanto a lista de ví
 os doadores e vítimas criam uma "ligação", enquanto os pedidos das vítimas que não formam uma ligação com algum doador, são notificados à 
 prefeitura
 *\
-}
